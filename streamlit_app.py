bbef9ebd8fff3b312ff0c79fecd069eef78e4b09
shruthikamath73 <137760053+shruthikamath73@users.noreply.github.com>
2023-06-26 13:37:16 +0530

    Update streamlit_app.py
    Adding multi select picker and changed Parents to Mom's in the title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,21 @@
 import streamlit
 import pandas
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom's New Healthy Diner')
+
 streamlit.header('Breakfast Menu')
+
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado toast')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
